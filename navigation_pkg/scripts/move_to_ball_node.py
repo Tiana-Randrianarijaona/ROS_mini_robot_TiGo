3ff1a5218066b45ddbb94c96af0b1653ff0d2572
Mandresy <randrianarijaonamandresy@gmail.com>
2024-04-15 21:58:50 +0200

    change the decision making of the robot making it move forward if it sees the ball, stop if the treshold distance is reached and turn around if ball untracked

--- navigation_pkg/scripts/move_to_ball_node.py
+++ navigation_pkg/scripts/move_to_ball_node.py
@@ -1,45 +1,48 @@
 #!/usr/bin/env python3
 
 import rospy  # import rospy
-from turtlesim.msg import Pose   # import Pose message 
 from geometry_msgs.msg import Twist   # import Twist message
 from math import atan2, sqrt  # import the mathematical functions atan2 and sqrt from the python module called math
 import sys
 from std_msgs.msg import Float32MultiArray
 
-global pub
-def pose_callback(msg):
-    global pub
-    distance = msg.data[0]
-    print(f"Distance = {str(distance)}")
-    vel = Twist()
-    if distance == 0.:        
-        vel.angular.z = 1.0 
-        pub.publish(vel)
-    # elif case_number == 2:
-    #     print("Case 2")
-    # elif case_number == 3:
-    #     print("Case 3")
+class NavigationToBall():
+    def __init__(self):
+        rospy.init_node('move_to_ball_node', anonymous=False)        
+        self.sub = rospy.Subscriber('/ball_data', Float32MultiArray, self.pose_callback)        
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        self.vel = Twist()
+        self.distance = 0
+        rate = rospy.Rate(10)
+        self.distanceTreshold = 20.
 
-def move_to_ball_node():
-    global pub
-    # Initialise the node
-    rospy.init_node('move_to_ball_node', anonymous=False)
-
-    # Create a subscriber to the raw_odom topic    
-    sub = rospy.Subscriber('/ball_data', Float32MultiArray, pose_callback)
-
-    # Create a publisher to the robot's velocity command
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-    rate = rospy.Rate(10)
-    rospy.spin()
-    # rate = rospy.Rate(10) # 10hz
- 
-    # vel = Twist() # creates a Twist object named vel
+    def pose_callback(self,msg):        
+        self.distance = msg.data[0]
+        print(f"Distance = {str(self.distance)}")        
+        if self.distance == 0.:        
+            self.selfRotate(0.3)
+        elif self.distance <= self.distanceTreshold:
+            self.stopRobot()
+        elif self.distance > self.distanceTreshold:
+            self.moveForward(0.2)
     
+    def selfRotate(self,angularSpeed):
+        self.vel.angular.z = angularSpeed
+        self.vel.linear.x = 0
+        self.pub.publish(self.vel)
+
+    def stopRobot(self):
+        self.vel.angular.z = 0
+        self.vel.linear.x = 0
+        self.pub.publish(self.vel)
+    
+    def moveForward(self,linearSpeed):
+        self.vel.angular.z = 0
+        self.vel.linear.x = linearSpeed
+        self.pub.publish(self.vel)
 if __name__ == '__main__':
     try:
-        move_to_ball_node()
-
+        NavigationToBall()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
