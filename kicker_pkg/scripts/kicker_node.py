740e1a0e24e36f2042a1f177c06f3bbcc016e9f3
Mandresy <randrianarijaonamandresy@gmail.com>
2024-05-26 15:37:19 +0200

    added the kickerPub attribute to subscribe to the kicking_decision topic and changed the callback function to send trigger message to the Arduino one the data from kicking_decision topic is true

--- kicker_pkg/scripts/kicker_node.py
+++ kicker_pkg/scripts/kicker_node.py
@@ -2,10 +2,10 @@
 import serial
 import time
 import rospy
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Bool
 
 class KickerNode():
-    def __init__(self,topicToSubscribe = '/kicker_trigger'):
+    def __init__(self,topicToSubscribe = '/kicking_decision'):
         self.ArduinoPort = '/dev/ttyUSB0'
         self.string_to_send  = "Tigo"     
         # Set up serial connection
@@ -13,21 +13,32 @@ class KickerNode():
         time.sleep(2)  # Allow some time for the serial connection to initialize
         rospy.init_node('image_subscriber')        
         self.topicToSubscribe = topicToSubscribe
-        self.subscriber = rospy.Subscriber(self.topicToSubscribe, Float32MultiArray, self.callback)
+        self.subscriber = rospy.Subscriber(self.topicToSubscribe, Bool, self.callback)
+        self.kickerPub = rospy.Publisher('/kicking_decision', Bool, queue_size = 10)
     
     def callback(self,msg):    
-        print(f"Just received the msg {msg.data}")        
+        print(f"Just received the msg {msg.data}")   
+        if(msg.data):
+            print("KICKER ON")
+            self.ser.write(self.string_to_send.encode())
+            time.sleep(100)
+            temp_Bool = Bool()
+            temp_Bool.data = False
+            self.kickerPub.publish(temp_Bool)
+            print("Trigger signal sent.")            
+        else:
+            print("WAITING TO KICK")        
         # Send the string
-        self.ser.write(self.string_to_send.encode())
-        print("Trigger signal sent.")        
+               
 
     def shutDown(self):
         # Close serial connection
         self.ser.close()
 
 if __name__ == '__main__':    
+    kickerNode = KickerNode() 
     try:
-        kickerNode = KickerNode() 
+        
         rospy.spin()
     except:
         pass
